1cae6e745113ec2f4cc914bed852188ddbb11228
Lucy McGowan <lucydagostino@gmail.com>
2021-10-04 13:07:06 -0400

    add build docs

---
+++ R/build.R
@@ -0,0 +1,5 @@
+# An optional custom script to run before Hugo builds your site.
+# You can delete it if you do not need it.
+lazyapero::upload_talk_md()
+lazyapero::upload_media_md()
+lazyapero::upload_paper_md()
